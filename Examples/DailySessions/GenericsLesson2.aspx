829653967c3e82ce317b11ef6d53902547399812
Ash Arya <aarya@pch.com>
2013-08-15 18:15:08 -0400

    Generics Lesson 2 - Restrictions

---
+++ Examples/DailySessions/GenericsLesson2.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" EnableViewState="false" CodeBehind="GenericsLesson2.aspx.cs" Inherits="DailySessions.GenericsLesson2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Generics Lesson</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Literal ID="myLiteralControl" runat="server"></asp:Literal><br />
+        <asp:Literal ID="myLiteralControl2" runat="server"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Examples/DailySessions/GenericsLesson2.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DailySessions
+{
+    public partial class GenericsLesson2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(FindLargest(new[] { 5, 28, 1, 7, 6 }));
+            Response.Write("<br />");
+            Response.Write(FindLargest(new[] { "a", "b", "c" }));
+            Response.Write("<br />");
+
+            var employees = new[]
+                                {
+                                    new Employee {Name = "Harry", Salary = 1000},
+                                    new Employee {Name = "Sally", Salary = 2000},
+                                    new Employee {Name = "Jeff", Salary = 3000}
+                                };
+            Response.Write(FindLargest(employees));
+        }
+
+        public class Employee : IComparable
+        {
+            public string Name { get; set; }
+            public int Salary { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("{0}: {1}", Name, Salary);
+            }
+
+            #region << IComparable Members >>
+
+            public int CompareTo(object obj)
+            {
+                var other = obj as Employee;
+                if (other == null) return 1;
+                return (Salary.CompareTo(other.Salary));
+            }
+
+            #endregion
+        }
+
+        public T FindLargest<T>(T[] array) where T : IComparable
+        {
+            if (array == null) return default(T);
+            var largest = default(T);
+            foreach (var i in array)
+            {
+                if (i.CompareTo(largest) > 0) largest = i;
+            }
+
+            return largest;
+        }
+    }
+}
